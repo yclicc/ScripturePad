2d71b43b70ed5d057ca0d93d9a67cd785a9f5197
yclicc <matthewbillson94@gmail.com>
2024-12-26 15:32:15 +0000

    Code formatting, added translate=no to menu options

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-import xss from 'xss';
-import { Marked } from 'marked';
-import { load } from 'std/dotenv/mod.ts';
-import { resolve } from 'std/path/mod.ts';
-import { walk } from 'std/fs/mod.ts';
-import { Router } from './router.ts';
-import { Paste, storage } from './storage.ts';
+import xss from "xss";
+import { Marked } from "marked";
+import { load } from "std/dotenv/mod.ts";
+import { resolve } from "std/path/mod.ts";
+import { walk } from "std/fs/mod.ts";
+import { Router } from "./router.ts";
+import { Paste, storage } from "./storage.ts";
 import {
   deletePage,
   editPage,
@@ -12,7 +12,7 @@ import {
   guidePage,
   homePage,
   pastePage,
-} from './templates.ts';
+} from "./templates.ts";
 
 interface TocItem {
   level: number;
@@ -23,81 +23,81 @@ interface TocItem {
 
 await load({ export: true });
 
-const SERVER_PORT = Deno.env.get('SERVER_PORT') ?? 8000;
-const API_KEY = Deno.env.get('API_KEY');
-const API_URL = 'https://4.dbt.io/api/';
+const SERVER_PORT = Deno.env.get("SERVER_PORT") ?? 8000;
+const API_KEY = Deno.env.get("API_KEY");
+const API_URL = "https://4.dbt.io/api/";
 const API_VERSION_NUMBER = 4;
-const STATIC_ROOT = resolve('./static');
+const STATIC_ROOT = resolve("./static");
 const FILES = new Map<string, string>();
 const MIMES: Record<string, string> = {
-  'js': 'text/javascript',
-  'css': 'text/css',
-  'ico': 'image/vnd.microsoft.icon',
+  js: "text/javascript",
+  css: "text/css",
+  ico: "image/vnd.microsoft.icon",
 };
 
 const XSS_OPTIONS = {
   whiteList: {
     ...xss.whiteList,
     // allow heading elements to have `id=` attributes
-    h1: ['id'],
-    h2: ['id'],
-    h3: ['id'],
-    h4: ['id'],
-    h5: ['id'],
-    h6: ['id'],
-    input: ['disabled', 'type', 'checked'],
+    h1: ["id"],
+    h2: ["id"],
+    h3: ["id"],
+    h4: ["id"],
+    h5: ["id"],
+    h6: ["id"],
+    input: ["disabled", "type", "checked"],
   },
 };
 
 for await (const file of walk(STATIC_ROOT)) {
   if (file.isFile) {
-    FILES.set('/' + file.name.normalize(), file.path);
+    FILES.set("/" + file.name.normalize(), file.path);
   }
 }
 
 const generateId = uid();
 const app = new Router();
 
-app.get('*', async (req) => {
+app.get("*", async (req) => {
   const url = new URL(req.url);
   const filepath = FILES.get(url.pathname);
 
   if (filepath) {
-    const [ext] = filepath.split('.').slice(-1);
-    const contentType = MIMES[ext] ?? 'text/plain';
+    const [ext] = filepath.split(".").slice(-1);
+    const contentType = MIMES[ext] ?? "text/plain";
     const file = await Deno.open(filepath, { read: true });
     const readableStream = file.readable;
     return new Response(readableStream, {
       status: 200,
       headers: {
-        'content-type': contentType,
+        "content-type": contentType,
       },
     });
   }
 });
 
-app.get('/', () => {
+app.get("/", () => {
   return new Response(homePage(), {
     status: 200,
-    headers: { 'content-type': 'text/html' },
+    headers: { "content-type": "text/html" },
   });
 });
 
-app.get('/guide', async () => {
-  const guideMd = await Deno.readTextFile('./guide.md');
+app.get("/guide", async () => {
+  const guideMd = await Deno.readTextFile("./guide.md");
   const parse = createParser();
   const { html, title } = parse(guideMd, { toc: false });
 
   return new Response(guidePage({ html, title }), {
     status: 200,
-    headers: { 'content-type': 'text/html' },
+    headers: { "content-type": "text/html" },
   });
 });
 
-app.get('/:id', async (_req, params) => {
-  let contents = '';
+app.get("/:id", async (_req, params) => {
+  let contents = "";
   let status = 200;
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const res = await storage.get(id);
 
   if (res.value !== null) {
@@ -118,15 +118,15 @@ app.get('/:id', async (_req, params) => {
   return new Response(contents, {
     status,
     headers: {
-      'content-type': 'text/html',
+      "content-type": "text/html",
     },
   });
 });
 
-app.get('/:id/edit', async (_req, params) => {
-  let contents = '';
+app.get("/:id/edit", async (_req, params) => {
+  let contents = "";
   let status = 200;
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const res = await storage.get(id);
 
   if (res.value !== null) {
@@ -142,16 +142,16 @@ app.get('/:id/edit', async (_req, params) => {
   return new Response(contents, {
     status,
     headers: {
-      'content-type': 'text/html',
+      "content-type": "text/html",
     },
   });
 });
 
-app.get('/:id/delete', async (_req, params) => {
-  let contents = '';
+app.get("/:id/delete", async (_req, params) => {
+  let contents = "";
   let status = 200;
 
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const res = await storage.get(id);
 
   if (res.value !== null) {
@@ -166,15 +166,15 @@ app.get('/:id/delete', async (_req, params) => {
   return new Response(contents, {
     status,
     headers: {
-      'content-type': 'text/html',
+      "content-type": "text/html",
     },
   });
 });
 
-app.get('/:id/raw', async (_req, params) => {
-  let contents = '';
+app.get("/:id/raw", async (_req, params) => {
+  let contents = "";
   let status = 200;
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const res = await storage.get(id);
 
   if (res.value !== null) {
@@ -189,32 +189,32 @@ app.get('/:id/raw', async (_req, params) => {
   return new Response(contents, {
     status,
     headers: {
-      'content-type': 'text/plain',
+      "content-type": "text/plain",
     },
   });
 });
 
-app.post('/save', async (req) => {
+app.post("/save", async (req) => {
   let status = 302;
-  let contents = '';
+  let contents = "";
   const headers = new Headers({
-    'content-type': 'text/html',
+    "content-type": "text/html",
   });
 
   const form = await req.formData();
-  const customUrl = form.get('url') as string;
-  const paste = form.get('paste') as string;
+  const customUrl = form.get("url") as string;
+  const paste = form.get("paste") as string;
   const slug = createSlug(customUrl);
 
-  let editCode: string | undefined = form.get('editcode') as string;
-  if (typeof editCode === 'string') {
+  let editCode: string | undefined = form.get("editcode") as string;
+  if (typeof editCode === "string") {
     editCode = editCode.trim() || undefined;
   }
 
   if (slug.length > 0) {
     const res = await storage.get(slug);
 
-    if (slug === 'guide' || res.value !== null) {
+    if (slug === "guide" || res.value !== null) {
       status = 422;
 
       contents = homePage({
@@ -225,22 +225,20 @@ app.post('/save', async (req) => {
     } else {
       await storage.set(slug, { paste, editCode });
       status = 302;
-      headers.set('location', '/' + slug.trim());
+      headers.set("location", "/" + slug.trim());
     }
   } else {
-    let id = '';
+    let id = "";
     let exists = true;
 
-    for (; exists;) {
+    for (; exists; ) {
       id = generateId();
-      exists = await storage.get(id).then(
-        (r) => r.value !== null,
-      );
+      exists = await storage.get(id).then((r) => r.value !== null);
     }
 
     await storage.set(id, { paste, editCode });
     status = 302;
-    headers.set('location', '/' + id.trim());
+    headers.set("location", "/" + id.trim());
   }
 
   return new Response(contents, {
@@ -249,44 +247,41 @@ app.post('/save', async (req) => {
   });
 });
 
-app.post('/:id/save', async (req, params) => {
-  let contents = '302';
+app.post("/:id/save", async (req, params) => {
+  let contents = "302";
   let status = 302;
 
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const form = await req.formData();
-  const paste = form.get('paste') as string;
-  let editCode: string | undefined = form.get('editcode') as string;
-  if (typeof editCode === 'string') {
+  const paste = form.get("paste") as string;
+  let editCode: string | undefined = form.get("editcode") as string;
+  if (typeof editCode === "string") {
     editCode = editCode.trim() || undefined;
   }
 
   const headers = new Headers({
-    'content-type': 'text/html',
+    "content-type": "text/html",
   });
 
   if (id.trim().length === 0) {
-    headers.set('location', '/');
+    headers.set("location", "/");
   } else {
     const res = await storage.get(id);
     const existing = res.value as Paste;
     const hasEditCode = Boolean(existing.editCode);
 
-    if (
-      hasEditCode &&
-      existing.editCode !== editCode
-    ) {
+    if (hasEditCode && existing.editCode !== editCode) {
       // editCode mismatch
       status = 400;
       contents = editPage({
         id,
         paste,
         hasEditCode,
-        errors: { editCode: 'invalid edit code' },
+        errors: { editCode: "invalid edit code" },
       });
     } else {
       await storage.set(id, { ...existing, paste });
-      headers.set('location', '/' + id);
+      headers.set("location", "/" + id);
     }
   }
 
@@ -296,41 +291,38 @@ app.post('/:id/save', async (req, params) => {
   });
 });
 
-app.post('/:id/delete', async (req, params) => {
-  let contents = '302';
+app.post("/:id/delete", async (req, params) => {
+  let contents = "302";
   let status = 302;
   const headers = new Headers({
-    'content-type': 'text/html',
+    "content-type": "text/html",
   });
 
-  const id = params.id as string ?? '';
+  const id = (params.id as string) ?? "";
   const form = await req.formData();
-  let editCode: string | undefined = form.get('editcode') as string;
-  if (typeof editCode === 'string') {
+  let editCode: string | undefined = form.get("editcode") as string;
+  if (typeof editCode === "string") {
     editCode = editCode.trim() || undefined;
   }
 
   if (id.trim().length === 0) {
-    headers.set('location', '/');
+    headers.set("location", "/");
   } else {
     const res = await storage.get(id);
     const existing = res.value as Paste;
     const hasEditCode = Boolean(existing.editCode);
 
-    if (
-      hasEditCode &&
-      existing.editCode !== editCode
-    ) {
+    if (hasEditCode && existing.editCode !== editCode) {
       // editCode mismatch
       status = 400;
       contents = deletePage({
         id,
         hasEditCode,
-        errors: { editCode: 'invalid edit code' },
+        errors: { editCode: "invalid edit code" },
       });
     } else {
       await storage.delete(id);
-      headers.set('location', '/');
+      headers.set("location", "/");
     }
   }
 
@@ -362,7 +354,7 @@ async function get_paginated(url, sortKey?: string, pageSize = 150) {
     const externalResponse = await fetch(
       `${url}&limit=${pageSize}&page=${page}`,
       {
-        method: 'GET',
+        method: "GET",
       },
     );
     const data = await externalResponse.json();
@@ -370,19 +362,20 @@ async function get_paginated(url, sortKey?: string, pageSize = 150) {
     allResults = allResults.concat(data.data);
 
     // Check if there are more pages
-    const total_pages = data.meta.pagination.total_pages || data.meta.pagination.last_page;
+    const total_pages =
+      data.meta.pagination.total_pages || data.meta.pagination.last_page;
     hasMore = total_pages > page;
     page += 1;
   }
 
-  if (typeof sortKey !== 'undefined') {
+  if (typeof sortKey !== "undefined") {
     allResults = sortByKey(allResults, sortKey);
   }
 
   const responseBody = JSON.stringify(allResults);
   return new Response(responseBody, {
     status: 200,
-    headers: { 'content-type': 'application/json' },
+    headers: { "content-type": "application/json" },
   });
 }
 
@@ -390,22 +383,22 @@ function hasWholeBible(bible) {
   let hasOt = false;
   let hasNt = false;
 
-  const filesets = bible.filesets?.['dbp-prod'] || [{ type: null }];
-  
+  const filesets = bible.filesets?.["dbp-prod"] || [{ type: null }];
+
   for (let item of filesets) {
-      if (["text_format", "text_plain"].includes(item.type)) {
-          let size = item.size;
-          if (size.includes("OT") && !size.includes("OTP")) {
-              hasOt = true;
-          } else if (size.includes("NT") && !size.includes("NTP")) {
-              hasNt = true;
-          } else if (size === "C") {
-              hasOt = true;
-              hasNt = true;
-          }
+    if (["text_format", "text_plain"].includes(item.type)) {
+      let size = item.size;
+      if (size.includes("OT") && !size.includes("OTP")) {
+        hasOt = true;
+      } else if (size.includes("NT") && !size.includes("NTP")) {
+        hasNt = true;
+      } else if (size === "C") {
+        hasOt = true;
+        hasNt = true;
       }
+    }
   }
-  
+
   return hasOt && hasNt;
 }
 
@@ -413,20 +406,20 @@ function collectCompleteBibles(data) {
   const completeBibles = {};
 
   for (let bible of data) {
-      const { language, autonym, iso, name, filesets } = bible;
+    const { language, autonym, iso, name, filesets } = bible;
 
-      if (hasWholeBible(bible)) {
-          if (!completeBibles[language]) {
-              completeBibles[language] = {
-                  autonym: autonym,
-                  iso: iso,
-                  bibles: {}
-              };
-          }
-
-          // Add the Bible name and its filesets to the appropriate language
-          completeBibles[language].bibles[name] = filesets;
+    if (hasWholeBible(bible)) {
+      if (!completeBibles[language]) {
+        completeBibles[language] = {
+          autonym: autonym,
+          iso: iso,
+          bibles: {},
+        };
       }
+
+      // Add the Bible name and its filesets to the appropriate language
+      completeBibles[language].bibles[name] = filesets;
+    }
   }
 
   // Sort languages alphabetically
@@ -436,27 +429,29 @@ function collectCompleteBibles(data) {
 
   // Sort the Bibles for each language alphabetically by Bible name
   for (let lang of sortedLanguages) {
-      const bibles = completeBibles[lang].bibles;
-      const sortedBibles = Object.keys(bibles).sort().reduce((sorted, bibleName) => {
-          sorted[bibleName] = bibles[bibleName];
-          return sorted;
+    const bibles = completeBibles[lang].bibles;
+    const sortedBibles = Object.keys(bibles)
+      .sort()
+      .reduce((sorted, bibleName) => {
+        sorted[bibleName] = bibles[bibleName];
+        return sorted;
       }, {});
 
-      sortedCompleteBibles[lang] = {
-          autonym: completeBibles[lang].autonym,
-          iso: completeBibles[lang].iso,
-          bibles: sortedBibles
-      };
+    sortedCompleteBibles[lang] = {
+      autonym: completeBibles[lang].autonym,
+      iso: completeBibles[lang].iso,
+      bibles: sortedBibles,
+    };
   }
 
   return sortedCompleteBibles;
 }
 
-app.get('/api/completebibles', async (req) => {
-  const cache = await caches.open('data-cache');
+app.get("/api/completebibles", async (req) => {
+  const cache = await caches.open("data-cache");
   const cached = await cache.match(req);
   if (cached) {
-    return cached
+    return cached;
   }
 
   const intermediate_response = await get_paginated(
@@ -464,32 +459,33 @@ app.get('/api/completebibles', async (req) => {
   );
   const data = await intermediate_response.clone().json();
 
-  const sortedCompleteBibles = collectCompleteBibles(data);
-  const responseBody = JSON.stringify(sortedCompleteBibles);
-  
   const res = new Response(responseBody, {
     status: 200,
-    headers: { 'content-type': 'application/json' },
+    headers: { "content-type": "application/json" },
   });
-  await cache.put(req, res.clone())
-  return res
+  await cache.put(req, res.clone());
+  return res;
 });
 
-app.get('/api/defaultbible', async (req, params, query) => {
-  const language = query.get('language_code');
-  const externalResponse = await fetch(
-    `${API_URL}bibles/defaults/types?language_code=${language}&key=${API_KEY}&v=${API_VERSION_NUMBER}`,
-    {
-      method: 'GET',
-    },
-  );
+app.get(
+  "/api/defaultbible",
+  async (req, params, query) => {
+    const language = query.get("language_code");
+    const externalResponse = await fetch(
+      `${API_URL}bibles/defaults/types?language_code=${language}&key=${API_KEY}&v=${API_VERSION_NUMBER}`,
+      {
+        method: "GET",
+      },
+    );
 
-  const responseBody = await externalResponse.text();
-  return new Response(responseBody, {
-    status: externalResponse.status,
-    headers: { 'content-type': 'application/json' },
-  });
-}, true);
+    const responseBody = await externalResponse.text();
+    return new Response(responseBody, {
+      status: externalResponse.status,
+      headers: { "content-type": "application/json" },
+    });
+  },
+  true,
+);
 
 Deno.serve({ port: Number(SERVER_PORT) }, app.handler.bind(app));
 
@@ -509,7 +505,7 @@ function createParser() {
   const marked = new Marked({ renderer, breaks: true });
   const parse = (markdown: string, { toc = true } = {}) => {
     let html = marked.parse(markdown) as string;
-    const title = tocItems[0] ? tocItems[0].text : '';
+    const title = tocItems[0] ? tocItems[0].text : "";
 
     if (toc) {
       const tocHtml = buildToc(tocItems);
@@ -522,39 +518,43 @@ function createParser() {
   return parse;
 }
 
-function createSlug(text = '') {
-  const lines = text.split('\n');
+function createSlug(text = "") {
+  const lines = text.split("\n");
 
   for (let i = 0; i < lines.length; i++) {
-    const slug = lines[i].toString().toLowerCase()
-      .replace(/\s+/g, '-') // Replace spaces with -
-      .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-      .replace(/\-\-+/g, '-') // Replace multiple - with single -
-      .replace(/^-+/, '') // Trim - from start of text
-      .replace(/-+$/, ''); // Trim - from end of text
+    const slug = lines[i]
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+      .replace(/\-\-+/g, "-") // Replace multiple - with single -
+      .replace(/^-+/, "") // Trim - from start of text
+      .replace(/-+$/, ""); // Trim - from end of text
 
     if (slug.length > 0) return slug;
   }
 
-  return '';
+  return "";
 }
 
 function uid() {
   // https://github.com/lukeed/uid
   // MIT License
   // Copyright (c) Luke Edwards <luke.edwards05@gmail.com> (lukeed.com)
-  let IDX = 36, HEX = '';
+  let IDX = 36,
+    HEX = "";
   while (IDX--) HEX += IDX.toString(36);
 
   return () => {
-    let str = '', num = 6;
-    while (num--) str += HEX[Math.random() * 36 | 0];
+    let str = "",
+      num = 6;
+    while (num--) str += HEX[(Math.random() * 36) | 0];
     return str;
   };
 }
 
 function buildToc(items: TocItem[] = []) {
-  let html = '';
+  let html = "";
 
   while (items.length > 0) {
     html += buildNestedList(items, 1);
@@ -564,7 +564,7 @@ function buildToc(items: TocItem[] = []) {
 }
 
 function buildNestedList(items: TocItem[] = [], level: number) {
-  let html = '<ul>';
+  let html = "<ul>";
 
   while (items.length > 0 && items[0].level === level) {
     const item = items.shift();
@@ -575,5 +575,5 @@ function buildNestedList(items: TocItem[] = [], level: number) {
     html += buildNestedList(items, level + 1);
   }
 
-  return html + '</ul>';
+  return html + "</ul>";
 }
--- static/viewer.js
+++ static/viewer.js
@@ -1,37 +1,37 @@
 // We import langs in to convert from the 3 character iso code used by dbt to
 // the two character machine translation one (and back)
-import langs from 'https://cdn.jsdelivr.net/npm/langs-es@4.0.2/+esm'
+import langs from "https://cdn.jsdelivr.net/npm/langs-es@4.0.2/+esm";
 
 function langMTtoDBT(inCode) {
-    // TODO Add other hacks for different edge languages
-    if (inCode == "zh-CN") {
-        // If simplified, assume Mandarin
-        return "cmn";
-    } else if (inCode == "zh-TW") {
-        // If traditional, assume "Cantonese"
-        return "yue";
-    } else if (inCode == "fa") {
-        // If Farsi, assume "Western Persian"
-        return "pes"
-    }
-    var outCode = langs.where("1", inCode)["3"];
-    if (outCode == "ara") {
-        outCode = "arb";
-    }
-    return outCode;
+  // TODO Add other hacks for different edge languages
+  if (inCode == "zh-CN") {
+    // If simplified, assume Mandarin
+    return "cmn";
+  } else if (inCode == "zh-TW") {
+    // If traditional, assume "Cantonese"
+    return "yue";
+  } else if (inCode == "fa") {
+    // If Farsi, assume "Western Persian"
+    return "pes";
+  }
+  var outCode = langs.where("1", inCode)["3"];
+  if (outCode == "ara") {
+    outCode = "arb";
+  }
+  return outCode;
 }
 
 function getPageLanguage(spec = "MT") {
-    // Spec stores whether we want a 2 letter machine translation code "MT"
-    // or a 3 letter code of the kind used by DBT, "DBT"
-    var lang = document.documentElement.lang;
-    if (spec == "MT") {
-        return lang;
-    } else if (spec = "DBT") {
-        return langMTtoDBT(lang);
-    } else {
-        throw new Error(`Spec ${spec} not implemented`);
-    }
+  // Spec stores whether we want a 2 letter machine translation code "MT"
+  // or a 3 letter code of the kind used by DBT, "DBT"
+  var lang = document.documentElement.lang;
+  if (spec == "MT") {
+    return lang;
+  } else if ((spec = "DBT")) {
+    return langMTtoDBT(lang);
+  } else {
+    throw new Error(`Spec ${spec} not implemented`);
+  }
 }
 
 // The language the page is currently known to be in. Should always be English to start.
@@ -42,52 +42,55 @@ const languageSelect = document.getElementById("language-select");
 const versionSelect = document.getElementById("version-select");
 
 async function populateLanguageDropdown() {
-    // Populate the language select with languages for which we have a
-    // complete Bible, then set the default to the current page language
-    const response = await fetch("/api/completebibles");
-    completeBibleData = await response.json();
+  // Populate the language select with languages for which we have a
+  // complete Bible, then set the default to the current page language
+  const response = await fetch("/api/completebibles");
+  completeBibleData = await response.json();
 
-    for (var languageName in completeBibleData) {
-        // Skip loop if the property is from prototype
-        if (!completeBibleData.hasOwnProperty(languageName)) continue;
+  for (var languageName in completeBibleData) {
+    // Skip loop if the property is from prototype
+    if (!completeBibleData.hasOwnProperty(languageName)) continue;
 
-        var language = completeBibleData[languageName];
-        
-        const opt = document.createElement('option');
-        opt.value = language.iso;
-        if ((language.autonym !== null) && (language.autonym !== languageName) ) {
-            opt.textContent = `${language.autonym} (${languageName})`;
-        } else {
-            opt.textContent = `${languageName}`;
-        }
-        languageSelect.appendChild(opt);
+    var language = completeBibleData[languageName];
+
+    const opt = document.createElement("option");
+    // Set the option element to translate="no" so that it doesn't get
+    // translated by Google Translate
+    opt.setAttribute("translate", "no");
+    opt.value = language.iso;
+    if (language.autonym !== null && language.autonym !== languageName) {
+      opt.textContent = `${language.autonym} (${languageName})`;
+    } else {
+      opt.textContent = `${languageName}`;
     }
-    languageSelect.value = getPageLanguage("DBT");
+    languageSelect.appendChild(opt);
+  }
+  languageSelect.value = getPageLanguage("DBT");
 }
 
 function onLanguageChange() {
-    // When we detect a change to the page language or on load, do this
-    languageSelect.value = getPageLanguage("DBT")
-    onLanguageSelect();
+  // When we detect a change to the page language or on load, do this
+  languageSelect.value = getPageLanguage("DBT");
+  onLanguageSelect();
 }
 
 function onLanguageSelect() {
-    // Populate the versionSelect dropdown (TODO)
+  // Populate the versionSelect dropdown (TODO)
 }
 
-document.addEventListener('DOMContentLoaded', populateLanguageDropdown);
+document.addEventListener("DOMContentLoaded", populateLanguageDropdown);
 
 // See https://martijnhols.nl/gists/how-to-detect-google-translate-and-other-machine-translation
 const languageObserver = new MutationObserver(() => {
-    const lang = document.documentElement.lang;
-    if (lang !== currentLanguage) {
-        currentLanguage = lang;
-        onLanguageChange();
-    }
+  const lang = document.documentElement.lang;
+  if (lang !== currentLanguage) {
+    currentLanguage = lang;
+    onLanguageChange();
+  }
 });
 languageObserver.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ['lang'],
-    childList: false,
-    characterData: false
+  attributes: true,
+  attributeFilter: ["lang"],
+  childList: false,
+  characterData: false,
 });
